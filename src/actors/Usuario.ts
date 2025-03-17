472179298e13a713e7082374611965fe977875f6
Cristian Esteban Torres Jimenez <127156743+Ctorres04@users.noreply.github.com>
2025-03-16 21:19:51 -0500

    Update Usuario.ts
    ajuste de comentarios

--- src/actors/Usuario.ts
+++ src/actors/Usuario.ts
@@ -5,14 +5,9 @@ import { CallAnApi } from '@serenity-js/rest';
 
 export class Usuario {
     static async queBuscaCrearUnaGuia() {
-        // Lanzar el navegador Chromium
         const browser = await chromium.launch({ headless: false });
-
-        // Crear un nuevo contexto y una nueva página
         const context = await browser.newContext();
         const page = await context.newPage();
-
-        // Retornar el actor con la capacidad de navegar usando Playwright
         return actorCalled('Usuario')
             .whoCan(BrowseTheWebWithPlaywright.using(page));
     }
